306e7e36310e819d501f7a3fb8bec89c9aefd39a
EliseH42 <elise.hofer42@gmail.com>
2021-12-01 08:44:58 -0700

    assignment completed

--- challenge-add-to-zero/addToZero.js
+++ challenge-add-to-zero/addToZero.js
@@ -1,33 +1,14 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 13]
-
-// Write your solution below:
-// function addToZero(array){
-//     for(let i=0;i<array.length;i++){
-//         for(let b=0;b<array.length;b++){
-//             let sumZero = false
-//             if(array[i]+array[b]===0){
-//                 sumZero = true
-//                 break;
-//             }else{
-//                 sumZero = false
-//             };
-//             return(sumZero)
-//         };
-//     };
-// };
+let array = [28, 43, -12, 30, 4, 0, 12]
 
 
 function addToZero(array){
-    // let inner = arr.concat(arr[0]);
     for (let i = 0; i<array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
         if (array[i] + array[j] === 0) return true;
         }
     }
     return false;
-    
 }
-
 
 console.log(addToZero(array))
